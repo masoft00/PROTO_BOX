05cc835d221aa3f07e862f2e99d84497ef6ce0f5
morseck00 <morseck00@gmail.com>
2020-01-05 19:14:24 +0000

    service authentification terminé

---
+++ AuthentificationJWT/models/user.model.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+const validator = require('validator')
+const user = mongoose.Schema({
+   _id: mongoose.Schema.Types.ObjectId,
+   prenom: {
+      type: String, 
+      required: true,
+      trim: true
+   },
+   nom: {
+      type: String, 
+      required: true,
+      trim: true
+   },
+   adresse: {
+      type: String, 
+      required: true,
+      trim: true
+   },
+   dateNaissance: {
+      type: Date, 
+      required: true,
+      trim: true
+   },
+   
+   email: {
+      type: String, 
+      required: true,
+      trim: true,
+      unique: true,
+      validate: value => {
+         if (!validator.isEmail(value)) {
+             throw new Error({error: "L'adresse email est invalide"})
+         }
+     }
+   },
+   password: {
+      type: String, 
+      required: true,
+      trim: true
+   }
+});
+
+module.exports = mongoose.model('User', user);
