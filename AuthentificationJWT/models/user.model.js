eb70ec9a59a10326606566ef0a6374b619d2c25c
morseck00 <morseck00@gmail.com>
2020-01-14 11:39:04 +0000

    supp node

--- AuthentificationJWT/models/user.model.js
+++ AuthentificationJWT/models/user.model.js
@@ -1,46 +1,11 @@
 const mongoose = require('mongoose');
+//module pour verifier la validité des entrés
 const validator = require('validator')
 
+//ici la structure de ma table (shema)
+const user = mongoose.Schema(
+    data.Tables.user
+);
 
-const user = mongoose.Schema({
-   _id: mongoose.Schema.Types.ObjectId,
-   prenom: {
-      type: String, 
-      required: true,
-      trim: true
-   },
-   nom: {
-      type: String, 
-      required: true,
-      trim: true
-   },
-   adresse: {
-      type: String, 
-      required: true,
-      trim: true
-   },
-   dateNaissance: {
-      type: Date, 
-      required: true,
-      trim: true
-   },
-   
-   email: {
-      type: String, 
-      required: true,
-      trim: true,
-      unique: true,
-      validate: value => {
-         if (!validator.isEmail(value)) {
-             throw new Error({error: "L'adresse email est invalide"})
-         }
-     }
-   },
-   password: {
-      type: String, 
-      required: true,
-      trim: true
-   }
-});
-
+//exporter mon module
 module.exports = mongoose.model('User', user);
--- AuthentificationJWT/server.js
+++ AuthentificationJWT/server.js
@@ -1,20 +1,20 @@
 // server.js
-const express    = require('express');
-const app        = express();
+const express = require('express');
+const app = express();
 const bodyParser = require('body-parser');
-const user       = require('./routes/user.route');
-const mongoose   = require('mongoose');
+const user = require('./routes/user.route');
+const mongoose = require('mongoose');
 
 //permer d lire et d'ecrire dans le fichier yaml
-const fs         = require('fs');
-const js_yaml    = require('js-yaml');
+const fs = require('fs');
+const js_yaml = require('js-yaml');
 
 //lecture du fichier
-const fichier    = fs.readFileSync('conf.yml');
-const data       = js_yaml.safeLoad(fichier);
+const fichier = fs.readFileSync('conf.yml');
+const data = js_yaml.safeLoad(fichier);
 
 //show yaml file
-mongoose.connect('mongodb://localhost/'+data.DatabaseName);
+mongoose.connect('mongodb://localhost/' + data.DatabaseName);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
@@ -28,6 +28,6 @@ app.use(bodyParser.json());
 app.use('/user', user);
 
 
-app.listen(data.PORT, function(){
-   console.log('Server is running on Port',data.PORT);
+app.listen(data.PORT, function() {
+    console.log('Server is running on Port', data.PORT);
 });
